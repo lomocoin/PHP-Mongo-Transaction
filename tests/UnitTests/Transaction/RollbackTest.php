1d5d1d45cb9b330148fceff731103707bf6ce028
Chen Shenghan <zzdjk6@gmail.com>
2018-02-08 21:55:30 +1300

    IMPROVE(Unit Test): add unit tests for rollback

---
+++ tests/UnitTests/Transaction/RollbackTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Lomocoin\Mongodb\Tests\UnitTests\Transaction;
+
+use Lomocoin\Mongodb\Config\TransactionConfig;
+use Lomocoin\Mongodb\Tests\TestCase;
+use Lomocoin\Mongodb\Transaction\Transaction;
+use MongoDB\Collection;
+
+class RollbackTest extends TestCase
+{
+    /**
+     * @var TransactionConfig
+     */
+    private static $config;
+
+    /**
+     * @var Collection
+     */
+    private static $testCollection;
+
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+
+        self::$config = self::getBasicConfig();
+
+        // share same database name with transaction log
+        $databaseName         = self::$config->getDatabaseName();
+        self::$testCollection = self::$config->getMongoDBClient()->$databaseName->unit_test;
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+        self::$testCollection->drop();
+    }
+
+    protected function tearDown()
+    {
+        self::$config->getStageChangeLogCollection()->drop();
+        self::$config->getTransactionCollection()->drop();
+        parent::tearDown();
+    }
+
+    public function testRollbackAll()
+    {
+        // baisc fixture
+        $objectIdA = self::$testCollection
+            ->insertOne([
+                'username' => 'A',
+                'email'    => 'a@example.com',
+                'name'     => 'AA',
+            ])
+            ->getInsertedId();
+
+        // test begin
+        $transaction = Transaction::begin(self::$config);
+
+        $objectIdB = $transaction
+            ->insertOne(self::$testCollection, [
+                'username' => 'B',
+                'email'    => 'b@example.com',
+                'name'     => 'BB',
+            ])
+            ->getInsertedId();
+
+        $objectIdC = $transaction
+            ->insertOne(self::$testCollection, [
+                'username' => 'C',
+                'email'    => 'c@example.com',
+                'name'     => 'CC',
+            ])
+            ->getInsertedId();
+
+        $transaction->updateOne(
+            self::$testCollection,
+            [
+                'username' => 'B',
+            ],
+            [
+                '$set' => [
+                    'name' => 'BBB',
+                ],
+            ]);
+
+        $transaction->deleteOne(self::$testCollection, ['username' => 'C']);
+
+        $this->assertEquals(2, self::$testCollection->count());
+
+        $objectA = self::$testCollection->findOne(['_id' => $objectIdA]);
+        self::assertEquals('A', $objectA['username']);
+        self::assertEquals('a@example.com', $objectA['email']);
+        self::assertEquals('AA', $objectA['name']);
+
+        $objectB = self::$testCollection->findOne(['_id' => $objectIdB]);
+        self::assertEquals('B', $objectB['username']);
+        self::assertEquals('b@example.com', $objectB['email']);
+        self::assertEquals('BBB', $objectB['name']);
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $transaction->rollback();
+
+        $this->assertEquals(1, self::$testCollection->count());
+
+        $objectA = self::$testCollection->findOne(['_id' => $objectIdA]);
+        self::assertEquals('A', $objectA['username']);
+        self::assertEquals('a@example.com', $objectA['email']);
+        self::assertEquals('AA', $objectA['name']);
+    }
+}
